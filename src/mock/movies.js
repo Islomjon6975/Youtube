aacc4392367e5082956934a9ca8b45db15219222
islomjon6975 <islomjon0919@mail.ru>
2022-03-03 22:24:35 +0500

    added

---
+++ src/mock/movies.js
@@ -0,0 +1,123 @@
+import webbrain1 from '../assets/movies/webbrain1.jpg'
+import webbrain2 from '../assets/movies/webbrain2.jpg'
+import titanic1 from '../assets/movies/titanik.jpg'
+import titanic2 from '../assets/movies/titanik2.jpg'
+import taxi from '../assets/movies/taxi.jpg'
+import programming1 from '../assets/movies/programming1.jpg'
+import programming2 from '../assets/movies/programming2.jpg'
+import ielts1 from '../assets/movies/ielts.jpeg'
+import ielts3 from '../assets/movies/ielts3.jpg'
+
+
+export const movie = [
+    {
+        id: 1,
+        name: 'Titanic - My Heart Will Go On (Music Video)',
+        img: titanic1,
+        category: 'movie'
+    },
+    {
+        id: 2,
+        name: 'JavaScript(Uzbek)',
+        img: webbrain1,
+        category: 'webbrain'
+    },
+    {
+        id: 3,
+        name: 'IELTS Speaking',
+        img: ielts1,
+        category: 'ielts'
+    },
+    {
+        id: 4,
+        name: 'Taxi',
+        img: taxi,
+        category: 'movie'
+    },
+    {
+        id: 5,
+        name: "HTML TO'LIQ DARSLIK (UZBEK) | HTML da ",
+        img: webbrain2,
+        category: 'webbrain'
+    },
+    {
+        id: 6,
+        name: 'Titanic Sinking (Sleeping Sun)',
+        img: titanic2,
+        category: 'movie'
+    },
+    {
+        id: 7,
+        name: 'Why Programming Is Important?',
+        img: programming1,
+        category: 'programming'
+    },
+    {
+        id: 8,
+        name: 'IELTS band 9',
+        img: ielts3,
+        category: 'ielts'
+    },
+    {
+        id: 9,
+        name: 'What A Day In A Programmers Life Is Actually Like',
+        img: programming2,
+        category: 'movie'
+    },
+    {
+        id: 10,
+        name: 'Titanic - My Heart Will Go On (Music Video)',
+        img: titanic1,
+        category: 'movie'
+    },
+    {
+        id: 12,
+        name: 'JavaScript(Uzbek)',
+        img: webbrain1,
+        category: 'webbrain'
+    },
+    {
+        id: 13,
+        name: 'IELTS Speaking',
+        img: ielts1,
+        category: 'ielts'
+    },
+    {
+        id: 14,
+        name: 'Taxi',
+        img: taxi,
+        category: 'movie'
+    },
+    {
+        id: 15,
+        name: "HTML TO'LIQ DARSLIK (UZBEK) | HTML da ",
+        img: webbrain2,
+        category: 'webbrain'
+    },
+    {
+        id: 16,
+        name: 'Titanic Sinking (Sleeping Sun)',
+        img: titanic2,
+        category: 'movie'
+    },
+    {
+        id: 17,
+        name: 'Why Programming Is Important?',
+        img: programming1,
+        category: 'programming'
+    },
+    {
+        id: 18,
+        name: 'IELTS band 9',
+        img: ielts3,
+        category: 'ielts'
+    },
+    {
+        id: 19,
+        name: 'What A Day In A Programmers Life Is Actually Like',
+        img: programming2,
+        category: 'movie'
+    },
+]
+
+
